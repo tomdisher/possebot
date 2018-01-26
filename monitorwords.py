a15b69cf8736f973c50465cb58c4c989e54c255b
Drewski <drewskiwooskie@gmail.com>
2018-01-26 13:55:49 -0500

    add joeyboy

--- monitorwords.py
+++ monitorwords.py
@@ -1,4 +1,4 @@
-import plugins
+import plugins, os
 
 def _initialise(bot):
     plugins.register_handler(_got_a_message, type="message", priority=50)
@@ -8,4 +8,18 @@ def _got_a_message(bot, event, command):
         yield from bot.coro_send_message(
             event.conv,
             _("YOU SAID THE SECRET WORDDDDDD!!!").format(
+                event.user.full_name, 'yay'))
+    elif "!joeyboy" in event.text.lower():
+        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+        rel_path = "joeyboy.txt"
+        joeyboywords_path = os.path.join(script_dir, rel_path)
+        words = open(joeyboywords_path, 'r')
+        words = words.read()
+        words = words.splitlines()
+        from random import randrange
+        random_index = randrange(0,len(words))
+        random_word = words[random_index]
+        yield from bot.coro_send_message(
+            event.conv,
+            _(random_word).format(
                 event.user.full_name, 'yay'))
