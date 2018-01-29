0ad817cea148f98c1c3d20c990e149f937e67a60
Drewski <drewskiwooskie@gmail.com>
2018-01-29 14:33:50 -0500

    pep8 fixes

--- possepersonpicture.py
+++ possepersonpicture.py
@@ -12,14 +12,18 @@ from bs4 import BeautifulSoup
 from fuzzywuzzy import process
 
 logger = logging.getLogger(__name__)
-members = ["joey", "meekle", "toomie", "gary", "alex", "miller", "bizic", "drewski", "keener"]
+members = ["joey", "meekle", "toomie", "gary",
+           "alex", "miller", "bizic", "drewski", "keener"]
+
 
 def sanitize_possemember(person):
-    return process.extractOne(person, members)[0];
+    return process.extractOne(person, members)[0]
+
 
 def _initialise(bot):
     plugins.register_user_command(["possepic"])
-    
+
+
 def possepic(bot, event, *args):
     site_url = bot.get_config_option('posseimage_url')
 
@@ -34,8 +38,8 @@ def possepic(bot, event, *args):
         soup = BeautifulSoup(text, "html.parser")
         images = soup.find_all('a', href=True)
     except urllib.error.HTTPError as e:
-        if e.code==404:
-            images=[]
+        if e.code == 404:
+            images = []
         else:
             yield from bot.coro_send_message(
                 event.conv,
@@ -43,29 +47,31 @@ def possepic(bot, event, *args):
                     event.user.full_name, 'yay'))
     from random import randrange
     if len(images) > 0:
-        random_index = randrange(0,len(images))
-        image_name=soup.find_all('a', href=True)[random_index]['href']
-        instanceImageUrl = "http://funny.drewstud.com/"+sanitized_member+"/"+image_name
+        random_index = randrange(0, len(images))
+        image_name = soup.find_all('a', href=True)[random_index]['href']
+        instanceImageUrl = "http://funny.drewstud.com/" +\
+            sanitized_member+"/"+image_name
         image_data = urllib.request.urlopen(instanceImageUrl)
         filename = os.path.basename(instanceImageUrl)
-        legacy_segments = [hangups.ChatMessageSegment( instanceImageUrl,
-                                                       hangups.SegmentType.LINK,
-                                                       link_target = instanceImageUrl )]
+        legacy_segments = [hangups.ChatMessageSegment(
+            instanceImageUrl,
+            hangups.SegmentType.LINK,
+            link_target=instanceImageUrl)]
         logger.debug("uploading {} from {}".format(filename, instanceImageUrl))
 
         try:
-            photo_id = yield from bot.call_shared('image_upload_single', instanceImageUrl)
+            photo_id = yield from bot.call_shared('image_upload_single',
+                                                  instanceImageUrl)
         except KeyError:
             logger.warning('image plugin not loaded - using legacy code')
-            photo_id = yield from bot._client.upload_image(image_data, filename=filename)
-    
-        yield from bot.coro_send_message(event.conv.id_, legacy_segments, image_id=photo_id)
+            photo_id = yield from bot._client.upload_image(image_data,
+                                                           filename=filename)
+
+        yield from bot.coro_send_message(event.conv.id_, legacy_segments,
+                                         image_id=photo_id)
 
     else:
         yield from bot.coro_send_message(
             event.conv,
             _("No images uploaded yet for %s" % dirty_member).format(
                 event.user.full_name, 'yay'))
-
-
-
